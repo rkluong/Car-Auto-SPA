4614e7966186c93f963592019d2a1b6055c06ccb
Randy <rkluong@gmail.com>
2018-02-17 08:36:45 -0800

    added db context

---
+++ CarWebSPA/Models/DBContext/Vehicles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarWebSPA.Models.DBContext
+{
+    public class Vehicles : DbContext
+    {
+        public DbSet<id> Id { get; set; }
+        public DbSet<year> year { get; set; }
+        public DbSet<make> make { get; set; }
+        public DbSet<model> model { get; set; }
+    }
+}
